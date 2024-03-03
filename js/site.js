6fbfc4c2f45e458103594873f87638a879b447a1
Beepered <bktrieu@gmail.com>
2024-03-02 16:57:51 -0800

    button change color

--- js/site.js
+++ js/site.js
@@ -15,6 +15,15 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  var elems = document.getElementsByClassName("flag-for-review");
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].onclick = function() {
+      var color = window.getComputedStyle(this, null)
+                  .getPropertyValue("background-color");
+      this.style.backgroundColor = color === "rgb(255, 255, 0)" 
+                                  ? "rgb(255, 165, 0)" : "rgb(255, 255, 0)";
+    };
+  };
 }
 
 // let's get this party started
