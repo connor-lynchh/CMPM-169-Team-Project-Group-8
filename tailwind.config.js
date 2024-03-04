65a81d24b0185b8d04ab0c2f07a9217adc9f13e6
nathan <000savior001@gmail.com>
2024-03-03 19:22:31 -0800

    Blog 5: Waifu Sighting SkinWalker added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Blog 5/**/*.{html,js}", 
+    "./Blog 6/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
