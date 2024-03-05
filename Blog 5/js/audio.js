772d15b77ee0144488ee5bd60e4c8821655d3651
nathan <000savior001@gmail.com>
2024-03-04 21:07:37 -0800

    switch background image button added

--- Blog 5/js/audio.js
+++ Blog 5/js/audio.js
@@ -3,12 +3,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const audioElement = document.getElementById('bgMusic');
     const pauseBtn = document.getElementById('pauseBtn');
 
+    const switchBtn = document.getElementById('switchBtn');
+    var imageDisplay = document.getElementById('imageDisplay');
+
+    var images = [
+        "./assets/forest1.png",
+        "./assets/forest2.jpeg",
+        "./assets/forest3.jpeg",
+        "./assets/forest4.jpeg",
+        "./assets/skinwalker.jpg"
+    ];
+    
+    var currentIndex = 0;
+
+    switchBtn.addEventListener('click', function(){
+        //cycle through array when click
+        currentIndex = (currentIndex + 1) % images.length;
+        imageDisplay.src = images[currentIndex];
+    });
+
     waifuIcon.addEventListener('click', function(){
         audioElement.pause();
         audioElement.src = './assets/memory.ogg';
         audioElement.load();
         audioElement.play();
-    })
+    });
 
     pauseBtn.addEventListener('click', function(){
         if(!audioElement.paused){
@@ -16,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             audioElement.play();
         }
-    })
+    });
 
-})
+    
+
+});
