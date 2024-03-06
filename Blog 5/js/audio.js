de21b6743ec44ea9c7f72f89758e93b9009adc03
nathan <000savior001@gmail.com>
2024-03-05 21:04:38 -0800

    more music easter eggs added

--- Blog 5/js/audio.js
+++ Blog 5/js/audio.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const waifuIcon = document.getElementById('waifuIcon');
+    const waifuIcon2 = document.getElementById('waifuIcon2');
+    const waifuIcon3 = document.getElementById('waifuIcon3');
+    const waifuIcon4 = document.getElementById('waifuIcon4');
+    const waifuIcon5 = document.getElementById('waifuIcon5');
+    const kikyo = document.getElementById('kikyoSama');
     const audioElement = document.getElementById('bgMusic');
     const pauseBtn = document.getElementById('pauseBtn');
 
@@ -24,7 +29,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
     waifuIcon.addEventListener('click', function(){
         audioElement.pause();
+        audioElement.src = '../Blog 6/assets/audio/aStepAway.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
+    waifuIcon2.addEventListener('click', function(){
+        audioElement.pause();
         audioElement.src = './assets/memory.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
+    waifuIcon3.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = '../Blog 6/assets/audio/forRiver.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
+    waifuIcon4.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = '../Blog 6/assets/audio/yanling.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
+    waifuIcon5.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = '../Blog 6/assets/audio/jiuyuansi.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
+    kikyoSama.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = '../Blog 6/assets/audio/dearest.ogg';
         audioElement.load();
         audioElement.play();
     });
--- Blog 6/js/audio.js
+++ Blog 6/js/audio.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // const waifuIcon = document.getElementById('waifuIcon');
+    const waifuIcon2 = document.getElementById('waifuIcon2');
+    const waifuIcon3 = document.getElementById('waifuIcon3');
+    const waifuIcon4 = document.getElementById('waifuIcon4');
+    const waifuIcon5 = document.getElementById('waifuIcon5');
+    const waifuIcon6 = document.getElementById('waifuIcon6');
     const audioElement = document.getElementById('bgMusic');
     const pauseBtn = document.getElementById('pauseBtn');
 
@@ -28,6 +33,38 @@ document.addEventListener('DOMContentLoaded', function() {
     //     audioElement.load();
     //     audioElement.play();//
     // })
+
+    waifuIcon2.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = 'assets/audio/aStepAway.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+    waifuIcon3.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = 'assets/audio/forRiver.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+    waifuIcon4.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = 'assets/audio/dearest.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+    waifuIcon5.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = 'assets/audio/shenhua.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+    waifuIcon6.addEventListener('click', function(){
+        audioElement.pause();
+        audioElement.src = 'assets/audio/jiuyuansi.ogg';
+        audioElement.load();
+        audioElement.play();
+    });
+
     audioElement.load();
     audioElement.play();
 
