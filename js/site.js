7de6503b6fecff661e2d70e2a74182b61f5a6462
Beepered <bktrieu@gmail.com>
2024-03-10 11:25:21 -0700

    super smart compressed "load local storage" function

--- js/site.js
+++ js/site.js
@@ -1,37 +1,9 @@
 // Constants
 
 // Functions
-//Brendan: reviewClick() and readClick() are not used for right now because I want to see if I can just use 1 function
-function reviewClick(button){
-  console.log("review");
-  button.style.backgroundColor = "#009F18";
-  /*
-  if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount) + 1; //needs to be Number() otherwise it is a string
-  } else {
-    localStorage.clickcount = 0;
-  }
-  console.log(localStorage.clickcount);
-  */
-}
 
-function readClick(button){
-  console.log("read");
-  button.style.backgroundColor = "#A0A0A0";
-  /*
-  if (localStorage.clickcount) {
-    localStorage.clickcount = Number(localStorage.clickcount) + 1;
-  } else {
-    localStorage.clickcount = 0;
-  }
-  console.log(localStorage.clickcount);
-  */
-}
-
-// btn must be this.id because it will remember the id of the button clicked
-// on button click save the button's ID to local storage
+// on button click: save the button's ID to local storage and change color
 function buttonClick(btnID, color){
-  console.log("e")
   localStorage.setItem(btnID, "true");
   const btn = document.getElementById(btnID);
   btn.style.backgroundColor = color;
@@ -39,41 +11,22 @@ function buttonClick(btnID, color){
 
 function loadLocalStorage(){
   console.log("begin local storage")
-  if(localStorage.getItem("button1")){
-    const btn = document.getElementById("button1");
-    btn.style.backgroundColor = "#009F18";
+  let revWord = "review";
+  for(var i = 1; i <= 9; i++){ //changing review buttons
+    let x = revWord + i;
+    if(localStorage.getItem(x)){
+      const btn = document.getElementById(x);
+      btn.style.backgroundColor = "#009F18";
+    }
   }
-  if(localStorage.getItem("button2")){
-    const btn = document.getElementById("button2");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button3")){
-    const btn = document.getElementById("button3");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button4")){
-    const btn = document.getElementById("button4");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button5")){
-    const btn = document.getElementById("button5");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button6")){
-    const btn = document.getElementById("button6");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button7")){
-    const btn = document.getElementById("button7");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button8")){
-    const btn = document.getElementById("button8");
-    btn.style.backgroundColor = "#009F18";
-  }
-  if(localStorage.getItem("button9")){
-    const btn = document.getElementById("button9");
-    btn.style.backgroundColor = "#009F18";
+
+  let readWord = "read";
+  for(var i = 1; i <= 9; i++){ //changing review buttons
+    let x = readWord + i;
+    if(localStorage.getItem(x)){
+      const btn = document.getElementById(x);
+      btn.style.backgroundColor = "#A0A0A0";
+    }
   }
 }
 
