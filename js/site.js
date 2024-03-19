db083d1b87f2b20fa6ac51cd7d4e2a6ef33b77c5
Beepered <bktrieu@gmail.com>
2024-03-19 11:13:22 -0700

    homepage now changes color and puts popup correctly

--- js/site.js
+++ js/site.js
@@ -48,7 +48,7 @@ function confirmFlag(btnID) { //when you click "Flag for review" but before actu
   document.getElementById('confirmationModal').style.display = 'flex';
 
   // Parse the blog number from the button ID
-  const blogNumber = btnID.replace('review', '');
+  //const blogNumber = btnID.replace('review', '');
 
   // When the user confirms, flag the post
   document.getElementById('confirmFlag').onclick = function() {
@@ -64,21 +64,18 @@ function closeModal() { //when the "confirmFlag() popup" disappears
   document.getElementById('confirmationModal').style.display = 'none';
 }
 
-function flagPost(blogNumber, boolean) { //when you click on "flag" or "cancel"
-  if(boolean){
-    //change colors
-    localStorage.setItem(blogNumber, "true");
-    const btn = document.getElementById(blogNumber);
+function flagPost(btnID, boolean) { //when you click on "flag" or "cancel"
+  if(boolean){    //change colors
+    localStorage.setItem(btnID, "true");
+    const btn = document.getElementById(btnID);
     btn.style.backgroundColor = "#009F18";
-    if(blogNumber.includes("review")){ //if button clicked is "Mark for review": change color of row
-      const row = document.getElementById("row" + blogNumber[6]);
-      row.style.backgroundColor = "#E5E4E2";
-      const row2 = document.getElementById("row" + blogNumber[6] + "a");
-      row2.style.backgroundColor = "#E5E4E2";
-    }
+    const row = document.getElementById("row" + btnID[6]);
+    row.style.backgroundColor = "#E5E4E2";
+    const row2 = document.getElementById("row" + btnID[6] + "a");
+    row2.style.backgroundColor = "#E5E4E2";
   }
   document.getElementById('confirmationModal').style.display = 'none';
-  generateReport(blogNumber, boolean);
+  generateReport(btnID.replace('review', ''), boolean);
 }
 
 function generateReport(blogId, flagged) {
@@ -98,7 +95,7 @@ function generateReport(blogId, flagged) {
         break;
     }
   } else {
-    switch(blogId) { //never shows up because generateReport only happens if you flag and not if you don't flag
+    switch(blogId) {
       case '1':
         reportMessage = "You chose not to flag the S.S. Eternity report. This could be an oversight if the phantoms manifest again.";
         break;
