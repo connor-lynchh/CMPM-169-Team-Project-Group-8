0af4ef7c697f96c93e572bb22befbb6ba2cc499e
Beepered <bktrieu@gmail.com>
2024-03-19 14:47:53 -0700

    change read button and moved stuff in site.js

--- js/site.js
+++ js/site.js
@@ -1,48 +1,6 @@
 // Constants
 
 // Functions
-
-// on button click for READ button: save the button's ID to local storage and change color
-function buttonClick(btnID, color){
-  localStorage.setItem(btnID, "true");
-  const btn = document.getElementById(btnID);
-  btn.style.backgroundColor = color;
-  /*
-  if(btnID.includes("review")){ //if button clicked is "Mark for review": change color of row
-    const row = document.getElementById("row" + btnID[6]);
-    row.style.backgroundColor = "#E5E4E2";
-    const row2 = document.getElementById("row" + btnID[6] + "a");
-    row2.style.backgroundColor = "#E5E4E2";
-    confirmFlag(btnID); // Call confirmFlag with the correct blog ID
-  }
-  */
-}
-
-function loadLocalStorage(){
-  console.log("begin local storage")
-  for(var i = 1; i <= 9; i++){ //changing review buttons
-    let x = "review" + i;
-    if(localStorage.getItem(x)){
-      const btn = document.getElementById(x);
-      btn.style.backgroundColor = "#009F18";
-
-      //change color of row
-      const row = document.getElementById("row" + i); //row next to buttons
-      row.style.backgroundColor = "#E5E4E2";
-      const row2 = document.getElementById("row" + i + "a"); //row with the buttons
-      row2.style.backgroundColor = "#E5E4E2";
-    }
-  }
-
-  for(var i = 1; i <= 9; i++){ //changing read buttons
-    let x = "read" + i;
-    if(localStorage.getItem(x)){
-      const btn = document.getElementById(x);
-      btn.style.backgroundColor = "#A0A0A0";
-    }
-  }
-}
-
 function confirmFlag(btnID) { //when you click "Flag for review" but before actually flagging or canceling
   // Show the confirmation modal
   document.getElementById('confirmationModal').style.display = 'flex';
@@ -58,12 +16,6 @@ function confirmFlag(btnID) { //when you click "Flag for review" but before actu
       flagPost(btnID, false);
   };
 }
-
-/*don't need to use this if it is just 1 line also "cancel" uses flagPost()
-function closeModal() { //when the "confirmFlag() popup" disappears
-  document.getElementById('confirmationModal').style.display = 'none';
-}
-*/
 
 function flagPost(btnID, boolean) { //when you click on "flag" or "cancel"
   if(boolean){    //change colors
@@ -129,6 +81,12 @@ function closeReportBox() {
   document.getElementById('reportBox').style.display = 'none';
 }
 
+function readButton(btnID){
+  localStorage.setItem(btnID, "true");
+  const btn = document.getElementById(btnID);
+  btn.style.backgroundColor = '#A0A0A0';
+}
+
 function resetFlagsAndReads() {
   // Loop through all possible buttons and reset their styles
   for (let i = 1; i <= 9; i++) {
@@ -161,7 +119,30 @@ function resetFlagsAndReads() {
   location.reload();
 }
 
+function loadLocalStorage(){
+  console.log("begin local storage")
+  for(var i = 1; i <= 9; i++){ //changing review buttons
+    let x = "review" + i;
+    if(localStorage.getItem(x)){
+      const btn = document.getElementById(x);
+      btn.style.backgroundColor = "#009F18";
 
+      //change color of row
+      const row = document.getElementById("row" + i); //row next to buttons
+      row.style.backgroundColor = "#E5E4E2";
+      const row2 = document.getElementById("row" + i + "a"); //row with the buttons
+      row2.style.backgroundColor = "#E5E4E2";
+    }
+  }
+
+  for(var i = 1; i <= 9; i++){ //changing read buttons
+    let x = "read" + i;
+    if(localStorage.getItem(x)){
+      const btn = document.getElementById(x);
+      btn.style.backgroundColor = "#A0A0A0";
+    }
+  }
+}
 
 function main() { //happens before any html object is created
   console.log("site.js working");
